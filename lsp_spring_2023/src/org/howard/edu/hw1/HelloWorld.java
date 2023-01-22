27d1fde7a888920e78cdbdd03c4ba35b6a56a17e
MilesDavies1 <104517485+MilesDavies1@users.noreply.github.com>
2023-01-22 14:55:39 -0500

    This is my first commit

---
+++ lsp_spring_2023/src/org/howard/edu/hw1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+*  Name: Miles Davies
+*/
+
+package org.howard.edu.hw1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+	}
+
+}
