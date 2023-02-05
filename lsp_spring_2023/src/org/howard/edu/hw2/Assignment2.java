2e6dd67fa359f9df0e3191f5f294b44d42d72aca
MilesDavies1 <104517485+MilesDavies1@users.noreply.github.com>
2023-02-05 15:17:25 -0500

    Assignment#2

---
+++ lsp_spring_2023/src/org/howard/edu/hw2/Assignment2.java
@@ -0,0 +1,40 @@
+package org.howard.edu.hw2;
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        File file = new File("src/main/resources/file.txt");//file to be read
+        HashMap<String, Integer> wordFrequency = new HashMap<>();
+        //String fileName = "file.txt";
+        //int wordCount = 0;
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {//takes in the file to be read
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Split the line into words
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    // Update the frequency of the word in the hash table
+                    wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);//adds word and frequency to the hash table
+                }
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error reading file " + file + ": " + e.getMessage());
+        }
+        
+        //Print the entire hash table
+        for (String word : wordFrequency.keySet()) {
+            System.out.println(word + ": " + wordFrequency.get(word));
+        }
+    }
+        
+	}
+
