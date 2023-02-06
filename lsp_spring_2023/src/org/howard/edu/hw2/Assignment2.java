99201eb62aa4a74eb2b2a6a164afc807c57a667a
MilesDavies1 <104517485+MilesDavies1@users.noreply.github.com>
2023-02-06 17:22:14 -0500

    Updated and completed code

--- lsp_spring_2023/src/org/howard/edu/hw2/Assignment2.java
+++ lsp_spring_2023/src/org/howard/edu/hw2/Assignment2.java
@@ -6,35 +6,33 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class Assignment2 { 
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        File file = new File("src/main/resources/file.txt");//file to be read
-        HashMap<String, Integer> wordFrequency = new HashMap<>();
-        //String fileName = "file.txt";
-        //int wordCount = 0;
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {//takes in the file to be read
-            String line;
-            while ((line = reader.readLine()) != null) {
-                // Split the line into words
-                String[] words = line.split("\\s+");
-                for (String word : words) {
-                    // Update the frequency of the word in the hash table
-                    wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);//adds word and frequency to the hash table
-                }
-            }
-            
-        } catch (IOException e) {
-            System.out.println("Error reading file " + file + ": " + e.getMessage());
-        }
-        
-        //Print the entire hash table
-        for (String word : wordFrequency.keySet()) {
-            System.out.println(word + ": " + wordFrequency.get(word));
-        }
-    }
-        
+public class Assignment2 {
+	  public static void main(String[] args) {
+	    // Create a hash table to store words and their frequencies
+	    HashMap<String, Integer> wordFrequency = new HashMap<>();
+	    String fileName = "src/main/resources/file.txt";
+	    
+	    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+	      String line;
+	      while ((line = reader.readLine()) != null) {
+	        // Split the line into words
+	        String[] words = line.split("\\s+");
+	        for (String word : words) {
+	          // Normalize the word to lowercase and remove numbers and punctuation
+	          word = word.toLowerCase().replaceAll("[^a-zA-Z]", "");
+	          
+	          // Update the frequency of the word in the hash table
+	          wordFrequency.put(word, wordFrequency.getOrDefault(word, 0) + 1);
+	        }
+	      }
+	    } catch (IOException e) {
+	      System.out.println("Error");
+	    }
+	    
+	    // Print the hash table
+	    for (String word : wordFrequency.keySet()) {
+	        System.out.println(word + " " + wordFrequency.get(word));
+	    }
+	  }
 	}
 
